ab1588d3c3c9544a70936c4e06f002c2070f649b
David Zabaleta <davidzabaletaf@gmail.com>
2020-01-12 15:50:48 -0500

    User story Single Minutes Row implemented
    Logic of the user story added and the unit tests passed. Should refactorice the logic.

--- src/main/java/com/sofka/berlin_clock_kata/BerlinClock.java
+++ src/main/java/com/sofka/berlin_clock_kata/BerlinClock.java
@@ -1,7 +1,25 @@
 package com.sofka.berlin_clock_kata;
 
 public class BerlinClock {
+
     public String singleMinutesRow(String time) {
-        return null;
+        int singleMinute  = Integer.parseInt(time.replace(":", "").substring(3,4));
+
+        switch (singleMinute){
+            case 1:
+            case 6:
+                return "YOOO";
+            case 2:
+            case 7:
+                return "YYOO";
+            case 3:
+            case 8:
+                return "YYYO";
+            case 4:
+            case 9:
+                return "YYYY";
+            default:
+                return "OOOO";
+        }
     }
 }
